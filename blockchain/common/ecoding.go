a50cd09351e3d9aebbf318b374ef3c2d72286725
Andrey <andrey@simiotics.com>
2024-02-07 17:09:45 +0200

    remove comments.

--- blockchain/common/ecoding.go
+++ blockchain/common/ecoding.go
@@ -1,51 +1,17 @@
 package common
 
+import "math/big"
+
 // BlockchainHandler defines the interface for handling blockchain data fetching.
 type BlockchainHandler interface {
-    FetchData(blockNumber *big.Int) ([]EventData, error)
+	FetchData(blockNumber *big.Int) ([]EventData, error)
 }
 
 // EventData outlines the structure for blockchain event data.
 type EventData struct {
-    // Common fields...
-    BlockNumber uint64
-    Data        []byte
-    ChainID     string
-    Extra       interface{} // Use for blockchain-specific additional data
+	// Common fields...
+	BlockNumber uint64
+	Data        []byte
+	ChainID     string
+	Extra       interface{} // Use for blockchain-specific additional data
 }
-
-// Additional common handler functionalities...
-
-
-
-// let's say we have a blockchain package that contains the following files:
-//
-// blockchain/
-// ├── common/
-// │   ├── decoding.go
-// │   └── encoding.go
-// ├── crawler/
-// │   └── crawler.go
-// ├── eth/
-// │   ├── decoding.go
-// │   └── encoding.go
-// ├── starknet/
-// │   ├── abi.go
-// │   ├── compression.go
-// │   └── starknet.go
-// ├── tezos/
-// │   ├── decoding.go
-// │   └── encoding.go
-// └── zilliqa/
-//     ├── decoding.go
-//     └── encoding.go
-//
-// We can use the following command to generate the documentation:
-//	godoc2md github.com/NethermindEth/blockchain-crawler > doc.md
-//	godoc2md github.com/NethermindEth/blockchain-crawler/common > common.md
-//	godoc2md github.com/NethermindEth/blockchain-crawler/crawler > crawler.md
-//	godoc2md github.com/NethermindEth/blockchain-crawler/eth > eth.md
-//	godoc2md github.com/NethermindEth/blockchain-crawler/starknet > starknet.md
-//	godoc2md github.com/NethermindEth/blockchain-crawler/tezos > tezos.md
-//	godoc2md github.com/NethermindEth/blockchain-crawler/zilliqa > zilliqa.md
-/
