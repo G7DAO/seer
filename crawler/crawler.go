0a0cd8d349232e608c0c7770ab2986c4ea8832e4
kompotkot <sumarokovsergei@yandex.ru>
2024-08-20 09:43:03 +0000

    Revert back maxWaitForBlocksTime

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -250,10 +250,7 @@ func (c *Crawler) Start(threads int) {
 	retryAttempts := 3
 	retryWaitTime := 5 * time.Second
 	waitForBlocksTime := retryWaitTime
-
-	/////// TODO!!
-	///// TODO
-	maxWaitForBlocksTime := 1 * retryWaitTime
+	maxWaitForBlocksTime := 24 * retryWaitTime
 
 	// If Start block is not set, using last crawled block from indexes database
 	if c.startBlock == 0 {
