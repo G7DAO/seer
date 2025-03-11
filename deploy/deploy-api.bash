c202c1560acc473eaa16d0694bad95d25d5f7474
kompotkot <sumarokovsergei@yandex.ru>
2025-03-11 13:12:08 +0000

    Modify permissions

--- deploy/deploy-api.bash
+++ deploy/deploy-api.bash
@@ -9,6 +9,9 @@ AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-us-east-1}"
 DEPLOYMENT_LOG_FILE_PATH="/home/$SYSTEM_USER/deployment.log"
 DEPLOY_SCRIPT_NAME="deploy.bash"
 
+echo "Modify permissions of app directory" >> "$DEPLOYMENT_LOG_FILE_PATH"
+chown -R ubuntu: "/home/$SYSTEM_USER/$INSTANCE_APP_NAME"
+
 echo "Retrieve latest deployment script" >> "$DEPLOYMENT_LOG_FILE_PATH"
 SCRIPT_FILE_PATH="/home/$SYSTEM_USER/$DEPLOY_SCRIPT_NAME"
 AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION" aws ssm get-parameter --name "/wb/deployment/$INSTANCE_APP_NAME/$DEPLOY_SCRIPT_NAME" --with-decryption --query "Parameter.Value" --output text > "$SCRIPT_FILE_PATH"
